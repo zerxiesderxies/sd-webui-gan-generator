3c1713e57f503d513fcf97ff59bb2208a08fcd6e
Bryz Quartz <quartzermia@gmail.com>
2024-02-27 13:16:50 -0600

    Removed Pillow from dependencies

--- install.py
+++ install.py
@@ -1,8 +1,6 @@
 import launch
 if not launch.is_installed("ninja"):
     launch.run_pip("install --upgrade ninja", "Requirement of GAN-Generator")
-if not launch.is_installed("Pillow"):
-    launch.run_pip("install --upgrade Pillow", "Requirement of GAN-Generator")
 if not launch.is_installed("scipy"):
     launch.run_pip("install --upgrade scipy", "Requirement of GAN-Generator")
 if not launch.is_installed("torchvision"):
